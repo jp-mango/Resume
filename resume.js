df94650be71f66da2addd60af366f4b165cc6845
Justin <menziesjustin@ymail.com>
2023-08-23 14:01:20 -0400

    Finished the home (resume) page

---
+++ resume.js
@@ -0,0 +1,17 @@
+const navbar = document.getElementById('navbar');
+let prevScrollPos = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.scrollY;
+
+    if (prevScrollPos > currentScrollPos) {
+        // Scrolling up, show the navbar
+        navbar.classList.remove('navbar-hidden');
+    } else {
+        // Scrolling down, hide the navbar
+        navbar.classList.add('navbar-hidden');
+    }
+
+    prevScrollPos = currentScrollPos;
+});
+
