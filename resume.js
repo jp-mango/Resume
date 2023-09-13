05fa045f008b9f9de9926af1d6cbab0cb92b6a3f
Justin <menziesjustin@ymail.com>
2023-09-13 13:41:02 -0400

    - Added countdown for projects page

--- resume.js
+++ resume.js
@@ -1,3 +1,4 @@
+//hides navbar when scrolling down
 const navbar = document.getElementById('navbar');
 let prevScrollPos = window.scrollY;
 
@@ -14,3 +15,25 @@ window.addEventListener('scroll', () => {
 
     prevScrollPos = currentScrollPos;
 });
+
+// countdown timer
+const launchDate = new Date("2023-11-23T00:00:00Z").getTime();
+
+// Update the countdown every second
+const countdown = document.getElementById("countdown");
+const timer = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = launchDate - now;
+
+    if (distance <= 0) {
+        clearInterval(timer);
+        countdown.innerHTML = "Website is live!";
+    } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+}, 1000);
